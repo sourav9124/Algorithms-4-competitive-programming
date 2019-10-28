4ebb8b77a77bee2d5f54aaa7f1445ac943641106
sourav singh <40472249+sourav9124@users.noreply.github.com>
2019-10-28 18:15:32 +0530

    Helpful for finding the gcd of two numbers of a large number.

---
+++ gcd of two numbers using Eucledean algorithm.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a,int b)
+{
+	if(a==0)
+	{
+		return b;
+	}
+	return gcd(b%a,a);
+}
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	int result=gcd(a,b);
+	cout<<result<<endl;
+}
